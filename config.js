ff1c20a696c6bd09b14c1f38749566298beace49
Remite7 <87864608+Remite7@users.noreply.github.com>
2023-11-29 17:50:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCPc3EFfJ2Iceqa0q4nlqq1e1Xda0RTzVY",
+  authDomain: "storytelling85-81fb5.firebaseapp.com",
+  databaseURL: "https://storytelling85-81fb5-default-rtdb.firebaseio.com",
+  projectId: "storytelling85-81fb5",
+  storageBucket: "storytelling85-81fb5.appspot.com",
+  messagingSenderId: "684752698239",
+  appId: "1:684752698239:web:c97d605683106bf641b84a"
+};
